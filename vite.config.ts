e4754148a7bcc6820cb3b4d6e6717d3b37c31f00
Maksim Klochkov <maksim.klochkov@rt.ru>
2024-08-26 18:25:23 +0300

    Update dependencies, ESLint, Vite, tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
+import path from 'node:path'
 import vue from '@vitejs/plugin-vue'
+
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  css: {
+    devSourcemap: true,
+  },
+  server: {
+    port: 3333,
+  },
 })
