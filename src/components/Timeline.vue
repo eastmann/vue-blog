a85f7683387c77ea49c1212a87a69cee10f1d326
Maksim Klochkov <maksim.klochkov@rt.ru>
2024-08-27 16:49:01 +0300

    Add Post type; Add posts to TimeLine component

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,12 +1,47 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
+import { DateTime } from 'luxon'
+// import type { Post } from '@/types/posts'
+import { thisMonth, thisWeek, today } from '@/types/posts'
 
+// constants
 const periods = ['Today', 'This Week', 'This Month'] as const
+const posts = [
+  today,
+  thisWeek,
+  thisMonth,
+]
 
 type Period = typeof periods[number]
 
+// data
 const selectedPeriod = ref<Period>('Today')
 
+// computed
+const formattedPosts = computed(() => {
+  return posts.map((post) => {
+    return {
+      ...post,
+      created: DateTime.fromISO(post.created),
+    }
+  })
+})
+
+const filteredPosts = computed(() => {
+  return formattedPosts.value.filter((post) => {
+    if (selectedPeriod.value === 'Today') {
+      return post.created >= DateTime.now().minus({ day: 1 })
+    }
+
+    if (selectedPeriod.value === 'This Week') {
+      return post.created >= DateTime.now().minus({ week: 1 })
+    }
+
+    return post
+  })
+})
+
+// methods
 function selectPeriod(period: Period) {
   selectedPeriod.value = period
 }
@@ -19,11 +54,20 @@ function selectPeriod(period: Period) {
       <a
         v-for="period in periods"
         :key="period"
-        :class="{ 'is-active': period === selectedPeriod }"
+        :class="period === selectedPeriod && 'is-active'"
         @click="selectPeriod(period)"
       >
         {{ period }}
       </a>
     </span>
+
+    <a
+      v-for="post of filteredPosts"
+      :key="post.id"
+      class="panel-block"
+    >
+      <a>{{ post.title }}</a>
+      <div>{{ post.created.toFormat('d MMM') }}</div>
+    </a>
   </nav>
 </template>
