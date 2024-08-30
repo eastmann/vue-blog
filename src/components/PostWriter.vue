bb417c246dfdf1a275594e743a3a4e8506cc5474
Maksim Klochkov <maksim.klochkov@rt.ru>
2024-08-30 18:13:14 +0300

    Creating PostWriter component

---
+++ src/components/PostWriter.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { TimelinePost } from '@/types/posts'
+
+interface ProsType {
+  post: TimelinePost
+}
+const props = defineProps<ProsType>()
+</script>
+
+<template>
+  <div class="columns">
+    <div class="column">
+      <div class="field">
+        <div class="label">{{ props.post.title }}</div>
+        <input type="text" class="input">
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/NewPost.vue
+++ src/views/NewPost.vue
@@ -1,10 +1,21 @@
 <script setup lang="ts">
+import { DateTime } from 'luxon'
 
+import PostWriter from '@/components/PostWriter.vue'
+
+import type { TimelinePost } from '@/types/posts'
+
+const post: TimelinePost = {
+  id: '-1',
+  title: 'Title',
+  created: DateTime.now(),
+}
 </script>
 
 <template>
   <div>
     New Post
+    <PostWriter :post="post" />
   </div>
 </template>
 
