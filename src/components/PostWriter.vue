00bf00a91baa7593a136ae93abac1345baa011a6
Maksim Klochkov <maksim.klochkov@rt.ru>
2024-09-03 13:59:32 +0300

    Two way binding with v-model

--- src/components/PostWriter.vue
+++ src/components/PostWriter.vue
@@ -1,18 +1,30 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import type { TimelinePost } from '@/types/posts'
 
+// props
 interface ProsType {
   post: TimelinePost
 }
+
 const props = defineProps<ProsType>()
+
+// data
+const title = ref(props.post.title)
 </script>
 
 <template>
-  <div class="columns">
-    <div class="column">
-      <div class="field">
-        <div class="label">{{ props.post.title }}</div>
-        <input type="text" class="input">
+  <div class="post-writer" data-ui-component="post-writer">
+    <div class="columns">
+      <div class="column">
+        <div class="field">
+          <div class="label">{{ props.post.title }}</div>
+          <input
+            v-model="title"
+            type="text"
+            class="input"
+          >
+        </div>
       </div>
     </div>
   </div>
